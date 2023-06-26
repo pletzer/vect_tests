a073e34f5404f2514663eb1fbfdc6fa01331d72d
Alex Pletzer <alexander@gokliya.net>
2023-06-27 10:51:15 +1200

    adding an example

---
+++ c_array/c_array.cxx
@@ -0,0 +1,33 @@
+#include <cstddef>
+#include <iostream>
+#include <ctime>
+
+int main(int argc, char* argv[]) {
+
+	// get the array size from the command line argument
+	std::size_t n = 100;
+	if (argc > 1) n = atol(argv[1]);
+
+	// allocate
+	double* a = new double[n];
+
+	time_t start, end;
+
+	// compute
+	time(&start);
+	double s = 0;
+	for (std::size_t i = 0; i < n; ++i) {
+		a[i] = i;
+		s += a[i];
+	}
+	time(&end);
+
+	// check
+	double sexact = n * (n - 1) / 2.;
+	std::cout << "s = " << s << " exact: " << sexact << " time: " << double(end - start) << " sec \n";
+
+	// clean up
+	delete[] a;
+
+	return 0;
+}
